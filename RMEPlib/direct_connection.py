85e76369551e2e937d41702c32801fdd18dc074a
DeltaZero <233a344a455@gmail.com>
2020-04-05 20:52:52 +0800

    Add exception handling

--- RMEPlib/direct_connection.py
+++ RMEPlib/direct_connection.py
@@ -2,50 +2,97 @@
 # coding=utf-8
 
 import socket
+import time
 import logger
 
 
 class CommendSender(object):
-    def __init__(self):
-        self.host = '127.0.0.1'  # '192.168.2.1' 用于测试
-        self.port = 40923
+    def __init__(self, host='192.168.2.1', port=40923, retry_interval=1):
+        self.host = host  # 用于测试
+        self.port = port
+        self.retry_interval = retry_interval
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect()
 
     def __del__(self):
         logger.info("Shuting down socket ...", self)
-        self.socket.shutdown(socket.SHUT_WR)
+        # self.socket.shutdown()
         self.socket.close()
 
-    def connect(self):
+    def connect(self, retry=3):
         logger.info("Connecting to %s:%s ..." % (self.host, self.port), self)
-        self.socket.connect((self.host, self.port))
-        logger.info("Control_commend port connected.", self)
 
-        for i in range(3):
-            recv = self.send("command")
-            if recv == 'OK':
-                logger.info("Entered commend mode successfully.", self)
-                break
+        try:
+            self.socket.connect((self.host, self.port))
+            logger.info("Control_commend port connected.", self)
+        except socket.error as e:
+            logger.warn("Fail to connect to S1. Error: %s" %e, self)
+            if retry > 0:
+                time.sleep(self.retry_interval)
+                logger.warn("Retrying...", self)
+                self.connect(retry-1)
             else:
-                logger.warn(
-                    "Unable to enter commend mode. Recevied: %s. Retrying %d ..." % (recv, i), self)
+                logger.error("Failed to retry", self)
+                self.connect(3)
+
+
+        # for i in range(3):
+        #     recv = self.send("command")
+        #     if recv == 'OK':
+        #         logger.info("Entered commend mode successfully.", self)
+        #         break
+        #     else:
+        #         time.sleep(self.retry_interval)
+        #         logger.warn(
+        #             "Unable to enter commend mode. Recevied: %s. Retrying %d ..." % (recv, i), self)
 
     def send(self, cmd):
+        """ Send a commend to S1.
+
+        Args:
+            cmd: (str) 命令
+        
+        Returns:
+            error_code: (int) 错误码
+                (0: 无错误，1: 发送时出错，2: 接收回应时出错)
+        """
         try:
             self.socket.send(cmd.encode('utf-8'))
-            recv = self.socket.recv(1024)
-            return recv.decode('utf-8')
         except socket.error as e:
-            logger.warn(
-                "An error occured at sending '%s': %s" % (cmd, e), self)
+            return 1, e
+
+        try:
+            recv = self.socket.recv(1024)
+            return 0, recv.decode('utf-8')
+        except socket.error as e:
+            return 2, e
+
+    def send_commend(self, cmd, n_retries=3):
+        error, response = self.send(cmd)
+        if error == 1:
+            logger.warn("Error at sending '%s': %s" %(cmd, response), self)
+        if error == 2:
+            logger.error("Error at receving the response of '%s': %s" %(cmd, response), self)
+        elif response == 'OK':
+            logger.info("'%s' recevied 'OK'" %cmd)
+            return True
+        else:
+            logger.warn("Recieved an error when executing '%s ': %s" %(cmd, response), self)
+
+        if n_retries > 0:
+            time.sleep(self.retry_interval)
+            logger.warn("Retrying...", self)
+            self.send_commend(cmd, n_retries-1)
+        else:
+            logger.error("Failed to retry", self)
+            self.send_commend(cmd, 3)
 
 
 if __name__ == "__main__":
-    sender = CommendSender()
+    sender = CommendSender(host='127.0.0.1')
     while True:
         cmd = input(">>> please input SDK cmd: ")
-        sender.send(cmd)
+        sender.send_commend(cmd)
 
         if cmd.upper() == 'Q':
             break
--- RMEPlib/logger.py
+++ RMEPlib/logger.py
@@ -7,23 +7,48 @@ def _get_class_name(who):
         return '[' + who.__class__.__name__ + ']'
 
 
-def info(msg, who=None):
-    class_name = _get_class_name(who)
-    print("\033[36m" + "[Info]%s: %s" % (class_name, msg) + "\033[0m")
-
-
-def warn(msg, who=None):
-    """Print warnings.
-
-    To replace warnings.warn() whose output is too complex.
-    用于替代输出复杂的 wanrnings.warn()
+def info(msg, where=None):
+    """Print infos.
 
     Args:
-        msg: (str) 输出的错误信息
-        who: (class / object) 用于定位warning产生的class
+        msg: (str) 输出的info信息
+        where: (class / object) 用于定位info产生的class
 
     Returns:
         None
     """
-    class_name = _get_class_name(who)
+    class_name = _get_class_name(where)
+    print("\033[36m" + "[Info]%s: %s" % (class_name, msg) + "\033[0m")
+
+
+def warn(msg, where=None):
+    """Print warnings.
+
+    Args:
+        msg: (str) 输出的警告信息
+        where: (class / object) 用于定位warning产生的class
+
+    Returns:
+        None
+    """
+    class_name = _get_class_name(where)
     print("\033[33m" + "[Warning]%s %s" % (class_name, msg) + "\033[0m")
+
+
+def error(msg, where=None):
+    """Print warnings.
+
+    Args:
+        msg: (str) 输出的错误信息
+        where: (class / object) 用于定位error产生的class
+
+    Returns:
+        None
+    """
+    class_name = _get_class_name(where)
+    print("=============================================")
+    print("\033[31m" + "[Error]%s %s" % (class_name, msg) + "\033[0m")
+    temp = input("Force to continue? (y/n) ")
+    print("=============================================")
+    if temp.upper() != 'Y':
+        exit()
