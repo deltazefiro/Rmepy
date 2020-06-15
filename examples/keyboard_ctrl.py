d631cf6c9c9e4e8179b2ef98ffb187c1f6bc481c
DeltaZero <233a344a455@gmail.com>
2020-06-15 12:00:36 +0800

    修复keyboard_ctrl

--- examples/keyboard_ctrl.py
+++ examples/keyboard_ctrl.py
@@ -10,13 +10,14 @@ import os
 import numpy as np
 import pygame
 import time
+import threading
 
 # add parent directory to import path
 sys.path.append(os.path.split(os.path.abspath(os.path.dirname(__file__)))[0])
 import rmepy
 
 MOVE_SPEED = 0.5
-ROTATE_SPEED = 20
+ROTATE_SPEED = 60
 CMD_SEND_FREQ = 5
 
 running = True
@@ -25,8 +26,11 @@ r.start()
 r.video.start()
 time.sleep(0.1)
 
+speed = (0, 0, 0)
+
 def key_handler(k):
     global running, r, speed
+    speed = (0, 0, 0)
     if k[pygame.K_w]:
         # forward
         speed = (MOVE_SPEED, 0, 0)
@@ -35,10 +39,10 @@ def key_handler(k):
         speed = (-MOVE_SPEED, 0, 0)
     if k[pygame.K_d]:
         # right
-        speed = (0, MOVE_SPEED, 0)
-    if k[pygame.K_f]:
-        # left
         speed = (0, -MOVE_SPEED, 0)
+    if k[pygame.K_a]:
+        # left
+        speed = (0, MOVE_SPEED, 0)
     if k[pygame.K_q]:
         # counter clockwise rotate
         speed = (0, 0, ROTATE_SPEED)
@@ -46,20 +50,24 @@ def key_handler(k):
         # clockwise rotate
         speed = (0, 0, -ROTATE_SPEED)
 
-def ctrl_thread():
+
+def ctrl_task():
+    global speed
     while running:
-        r.chassis.set_speed(speed)
+        r.chassis.set_speed(*speed)
         time.sleep(1/CMD_SEND_FREQ)
 
 if __name__ == "__main__":
+    ctrl_thread = threading.Thread(target=ctrl_task)
+    ctrl_thread.start()
+
     pygame.init()
 
-    display = pygame.display.set_mode((1280, 720))
+    display = pygame.display.set_mode((1080, 720))
     clock = pygame.time.Clock()
 
     while running:
 
-        speed = (0, 0, 0)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -70,8 +78,9 @@ if __name__ == "__main__":
         # Draw image
         temp = r.video.get_frame()
         if temp is not None:
-            frame = temp
-        pygame.surfarray.blit_array(display, frame)
+            frame = np.rot90(temp, 1)
+        surf = pygame.surfarray.make_surface(frame)
+        display.blit(surf, (0, 0))
         pygame.display.flip()
 
         clock.tick(30)
